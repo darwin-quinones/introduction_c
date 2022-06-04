b6f0c2d03b015e072b5af7fe75dde68591640473
darwin-quinones <darwinquinones735@gmail.com>
2022-06-04 12:02:45 -0500

    First commit after installed git

---
+++ programa/programa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!. Darwin's programming");
+
+            // Variables
+
+            double height = 181;
+            int age = 20;
+            string nombre = "Darwin Quiñones Sanchez";
+            string placeHolderText = "The information you requested is: ";
+            string information = placeHolderText + height + " "+ age + " " + nombre;
+            Console.WriteLine(information);
+        }
+    }
+}
